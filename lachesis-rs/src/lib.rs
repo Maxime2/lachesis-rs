0706d77f9a8c1faea35f0d66e1030e98af0a787d
Maxim Zakharov <dp.maxime@gmail.com>
2019-01-07 14:10:55 +1100

    dummy lachesis-rs : switch to 2018 edition

--- lachesis-rs/src/lib.rs
+++ lachesis-rs/src/lib.rs
@@ -1,17 +1,11 @@
-extern crate base64;
-extern crate bincode;
 #[macro_use]
 extern crate failure;
 #[macro_use]
 extern crate log;
 #[macro_use]
 extern crate proptest;
-extern crate rand;
-extern crate ring;
-extern crate serde;
 #[macro_use]
 extern crate serde_derive;
-extern crate untrusted;
 
 macro_rules! get_from_mutex {
     ($resource: expr, $error: ident) => {
